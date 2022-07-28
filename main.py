ac8b222593ea68db32caacf9c06d4edd6700cbf3
Fuechee <f9vang@gmail.com>
2022-07-27 19:24:01 -0500

    Updated main.py with locations, restaurants, transportation, and entertainment

---
+++ main.py
@@ -0,0 +1,25 @@
+#Destination
+destinations = ['Tokyo', 'Paris', 'Seoul', 'Syndey', 'honolulu', 'New York', 'Barcelona']
+
+#Restaurants
+tokyo_restaurants = ['Sukiyabashi Jiro Roppongi', 'Den', 'Bistro Shirube', 'Takahashi', 'Kanda Matsuya', 'Nigyocho Imahan', 'Kaneko Hannosuke']
+paris_restaurants = ['Tawlet Paris', 'Bouche', 'Cafe de Luce', 'Collier de la Reine', 'Cafe Lignac', 'Substance', 'Le Mary Celeste']
+seoul_restaurants = ['Na Jeong-sun Halmae Jjukkumi', 'Han Chu', 'Hanok Jip', 'Ejo', 'Balwoo Gongyang', 'Gwanghwamun Jip', 'Mapo Jeong Daepo']
+syndey_restaurants = ['Quay', 'est.', 'Sydney Tower Buffet', "Tetsuya's", 'Bills', "Jamie's Italian Sydney", 'Rockpool Bar and Grill']
+honolulu_restaurants = ['House Without A Key', "Merriman's", 'MW Restaurant', 'Mud Hen Water', 'The Pig and The Lady', "Helena's Hawaiian Food", 'Piggy Smalls']
+new_york_restaurants = ['Burger Joint', 'The Halal Guys', 'Dommminique Ansel Bakery', 'Balthazar', "Katz's Delicatessen", 'Peter Luger Steak House', "Grimaldi's Pizza"]
+barcelona_restaurants = ['Bar Canete', 'Martinez', 'Bar Brutal', 'Bodega La Puntual', 'Bar Morryssom', 'Parking Pizza', 'Salvaje']
+
+#Transportation
+transportation = ['Rental Car', 'Train', 'Rental Bike', 'Walking']
+
+#Entertainment 
+tokyo_entertainment = ['Shinjuku Gyoen National Garden', 'Ueno Park and Ueno Zoo', 'Gina District', 'Tokyo Skytree']
+new_york_entertainment = ['Statue of Liberty', 'Central Park', 'Metropolitan Museum of Art', 'Time Square']
+paris_entertainment = ['Eiffel Tower', 'Musee du Louvre', 'Cathedrale Notre-Dame de Paris', 'Avenue des Champs-Elysees']
+seoul_entertainment = ['N Seoul Tower', 'Dongdaemun Design Plaza', 'Lotte World Tower', 'Gwangjang Market']
+syndey_entertainment = ['Syndey Opera House', 'Sydney Harbour Bridge', 'Royal Botanic Garden Syndey', 'Bondi Beach']
+honolulu_entertainment = ['Wakikiki Beach', 'Aloha Tower', 'USS Missouri Battleship', 'Polynesian Cultural Center']
+barcelona_entertainment = ['Sagrada Familia', 'Park Guell', 'Las Ramblas', 'Parc de Montjic and Magic Fountain']
+
+
